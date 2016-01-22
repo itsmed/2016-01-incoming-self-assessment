7d4828558efa071fd6c177ae51a9fc705113bba6
Dejon Gill <dejonjgill@gmail.com>
2016-01-21 16:25:34 -0800

    Completed binarySearch

--- binarySearchArray/binarySearch.js
+++ binarySearchArray/binarySearch.js
@@ -9,5 +9,80 @@
  */
 
 var binarySearch = function (array, target) {
- // your code here
+	
+	//index variable to hold the index of target, start and end to use in recursive call
+	var index, start, end;
+
+	//variable middleIndex to take the middle index of array
+	var middleIndex = Math.floor(array.length / 2);
+
+	//if the value at middle index equals target
+	if (array[middleIndex] === target) {
+
+		//assign middleIndex to index and return index
+		index = middleIndex;
+
+		return index;
+
+	}
+
+	//if value at target if greater than value at middle index
+	if (target > array[middleIndex]) {
+
+		//assign middleIndex to start, and end gets array.length - 1
+		start = middleIndex;
+		end = array.length -1;
+
+	}
+
+	//if value at target is smaller than value at middle index
+	if (target < array[middleIndex]) {
+
+		//assign 0 to start, and middleIndex to end
+		start = 0;
+		end = middleIndex;
+	}
+
+	//function to recursively call
+	function find(start, end) {
+		
+		var midIndex = Math.floor(end - start /2);
+		console.log('mid', midIndex);
+		//if value at midIndex equals value at target
+		if (array[midIndex] === target) {
+
+			//return midIndx
+			return midIndex;
+		}
+
+		//if target is greater than the value at midIndex
+		else if(target > array[midIndex]) {
+
+			console.log(array[midIndex]);
+			//assign midIndex to start, and end gets array.length-1
+			start = midIndex;
+			end = array.length-1;
+			// return find(start, end);
+			console.log('s', start, 'e', end);
+		}
+
+		//if value at target is smaller than value at midIndex
+		else if(target < array[midIndex]) {
+			console.log(array[midIndex]);
+			//assign 0 to start and midIndex to end
+			start = 0;
+			end = midIndex;
+			// return find(start, end);
+			console.log('s', start, 'e', end);
+		}
+
+		else {
+			return null;
+		}
+
+
+	}
+
+	return find(start, end);
+
 };
