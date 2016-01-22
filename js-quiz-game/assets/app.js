40894de01265803cc64b11b9374d442bdbf4653a
Dejon Gill <dejonjgill@gmail.com>
2016-01-21 18:46:22 -0800

    AnswerGenerator may be close to complete

--- js-quiz-game/assets/app.js
+++ js-quiz-game/assets/app.js
@@ -7,20 +7,24 @@ $(document).ready(function(){
 	var $currentForm = $('form');
 	// creates our first random question upon generating the game.html file
 	var firstRandomQuestion = app.questions[Math.floor(Math.random()* app.questions.length)];
-	console.log(firstRandomQuestion);
+	
 	//attaches the counter to the highscore on page
 	$('#counter').text(app.count);
-	
+
 	//attaches the first question to the form onload
 	$currentForm.prepend('<h3 class = "question"> Question: ' + firstRandomQuestion.question + '</h3>' + '<br><br>');
 
 	// appends all four answers to the corresponding question
 	var answerGenerator = function (randomQuestion){
-		var $possibleAnswers = $('<ul></ul>');
-		for (var i = 0; i < firstRandomQuestion.choices.length; i++) {
-			$possibleAnswers.text('<li>' + firstRandomQuestion.choices[i] + '</li>');
+
+		var that = randomQuestion;
+		console.log(that.correct);
+		for (var i = 0; i < that.choices.length; i++) {
+		
+		var $answer = $('<div class"answer"><input type="radio" name="user_answer">'+ '&nbsp &nbsp' + that.choices[i]+'</input></div>');
+
+			$currentForm.append($answer);
 		}
-		$possibleAnswers.appendTo('.choices');
 	};
 
 	//adds a random question and its corresponding answers to our currentForm
@@ -32,7 +36,7 @@ $(document).ready(function(){
 	$('#checkAnswer').on('click',function(){
 		event.preventDefault();
 		// represents whichever check box the user clicks on
-		$userInput = $('input:checked');
+		$userInput = $('user_answer');
 
 		if(app.randomQuestion.choices[$userInput.val()] === app.randomQuestion.correct ){
 			app.successDisplay();
